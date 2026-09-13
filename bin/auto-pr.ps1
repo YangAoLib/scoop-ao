dc03ef9c388a42b554a1b7c07d3bd5a40742d718
YangAo <ya1398789131@163.com>
2026-09-14 00:58:19 +0800

    feat: initialize bucket with KOOK

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "YangAoLib/scoop-ao:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
